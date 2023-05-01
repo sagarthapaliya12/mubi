03d85c9c7612eafd7c302a2772edf46f5267a9d1
Sagar Raj Thapaliya <sr3959596@gmail.com>
2023-05-01 18:20:19 +0545

    developed Home Page

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                Mubi is a Free Movies streaming site with zero ads. We let you watch movies online without having to register or paying, with over 10000 movies and TV-Series. You can also Download full movies from MoviesCloud and watch it later if you want.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
